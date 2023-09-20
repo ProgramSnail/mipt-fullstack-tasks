0f1a2ea8e26fefd1798fb7c73a656c7ea5d68d5c
ProgramSnail <PurpureBacterium@yandex.ru>
2023-09-20 22:21:59 +0300

    images, style classes added (v3)

--- task1/index.js
+++ task1/index.js
@@ -7,7 +7,7 @@ function update_factory(coffee) {
       if (choosen_coffee != -1) document.getElementById(choosen_coffee).style.display = 'none';
       else if (entry.checked) description.style.visibility = 'visible';
       choosen_coffee = entry.checked ? coffee : -1
-      if (choosen_coffee != -1) document.getElementById(choosen_coffee).style.display = 'contents';
+      if (choosen_coffee != -1) document.getElementById(choosen_coffee).style.display = 'inherit';
       else description.style.visibility = 'hidden';
     } else {
       entry.checked = false
